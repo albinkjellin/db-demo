b34aa5ddf75944756ace17acd9d22ddb6f75f3dd
albinkjellin <albin.kjellin@mulesoft.com>
2012-10-25 14:57:07 -0700

    initial commit

---
+++ src/main/resources/db/notes.sql
@@ -0,0 +1,12 @@
+insert into`test`.`person` (id, firstname, lastname, age, address, city, postalcode, country) values ('1', 'Albin', 'Kjellin', 30, '525 Nelson Rising Ln', 'San Francisco', '94158', 'us');
+insert into`test`.`person` (id, firstname, lastname, age, address, city, postalcode, country) values ('2', 'Dave', 'Thomson', 32, '77 Geary Street', 'San Francisco', '94123', 'us');
+
+
+id               varchar(255) PK
+firstname        varchar(255)
+lastname         varchar(45)
+age              int(11)
+	          varchar(255)
+city             varchar(45)
+postalcode       varchar(45)
+country          varchar(45)
